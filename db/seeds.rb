22e1dd05140cf0a9ef4ffb42970387cb49832c70
Алдияр Примбаев <primbayev.96aldi@gmail.com>
2019-03-30 19:54:23 +0600

    Написал сиды

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.create!(
+  name: 'admin',
+  email: 'admin@exampl.com',
+  admin: true,
+  active: true,
+  password: 'qweasd',
+  password_confirmation: 'qweasd'
+)
+
+5.times do
+  user = User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.unique.email,
+    admin: false,
+    active: true,
+    password: 'qweasd',
+    password_confirmation: 'qweasd'
+  )
+  category = Category.create!(
+    title: Faker::Name.name,
+    description: Faker::Lorem.sentence
+  )
+
+  place = Place.create!(
+    title: Faker::Name.name,
+    description: Faker::Lorem.sentence,
+    category_id: category.id,
+    user_id: user.id,
+    terms: true,
+    active: true
+  )
+end
+
