23e630126de1b347ec5f2087098cedf8e372c97b
Алдияр Примбаев <primbayev.96aldi@gmail.com>
2019-03-30 13:06:07 +0600

    Написал валидации для модели category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   has_many :places
+
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { maximum: 150 }
 end
