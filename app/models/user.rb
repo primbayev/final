50aeb69064aa1b690446ed4bd794ae5b8b1b25b5
Алдияр Примбаев <primbayev.96aldi@gmail.com>
2019-03-30 14:07:39 +0600

    Связал модель user и place. Один-ко-многим

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :places
+
 
   validates :name, presence: true, length: { maximum: 50 }
 end
