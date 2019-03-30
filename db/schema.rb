9dc0e07ed6a6f09d3393ccd27d6717e58af1c606
Алдияр Примбаев <primbayev.96aldi@gmail.com>
2019-03-30 12:40:38 +0600

    Создал модели category и place

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_30_053458) do
+ActiveRecord::Schema.define(version: 2019_03_30_063530) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_places_on_category_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
