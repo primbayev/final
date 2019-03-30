9ccee40d0a6fbd9ff79aeb8748df16d7e51ae950
Алдияр Примбаев <primbayev.96aldi@gmail.com>
2019-03-30 14:08:31 +0600

    Написал валидации для модели place

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :category
   belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { maximum: 150 }
 end
