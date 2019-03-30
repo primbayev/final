34b5140cdff72be8f6363cac24cac06edcff0ae5
Алдияр Примбаев <primbayev.96aldi@gmail.com>
2019-03-30 15:27:58 +0600

    Прописал валидации, теперь один и тот же пользователь не может комментировать одно и то же заведение

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
+
+  validates :body, presence: true, length: { maximum: 150 }
+  validates_uniqueness_of :user_id, scope: [:place_id]
 end
